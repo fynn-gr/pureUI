1e35335a5f69764dc121145d5b4ecf29275c7f30
Fynn Grotehans <68659993+Fynn-G@users.noreply.github.com>
2022-12-30 00:01:10 +0100

    button bar buttons

--- test/components/TopBarContentSlim.tsx
+++ test/components/TopBarContentSlim.tsx
@@ -7,6 +7,16 @@ export default function Properties() {
 				<button class="">Screen</button>
 				<button class="">Color</button>
 			</div>
+
+			<div class="buttonBarSelector">
+				<button class="buttonBarButton">
+					<object class="icon" data="../../src-tauri/icons/tb_sidebar.svg"></object>
+				</button>
+				<button class="buttonBarButton active">
+					<object class="icon" data="../../src-tauri/icons/tb_play.svg"></object>
+				</button>
+			</div>
+
 			<div style="flex: 1;" data-tauri-drag-region></div>
 		</div>
 	);
