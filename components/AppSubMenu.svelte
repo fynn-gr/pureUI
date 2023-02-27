2a100346815ed0be529c129dc3eb9304377b9758
Fynn Grotehans <68659993+Fynn-G@users.noreply.github.com>
2023-02-27 20:21:38 +0100

    header, submenu

---
+++ components/AppSubMenu.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+
+    export let name: string;
+
+</script>
+
+<div class="aubMenu">
+    <button class="subMenuBtn">{name}</button>
+    <div class="menuContent">
+      <slot />
+    </div>
+  </div>
