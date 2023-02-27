7cdd422ea8e57f6f115a2d885dc04ce056c6b2c2
Fynn Grotehans <68659993+Fynn-G@users.noreply.github.com>
2023-02-27 12:08:50 +0100

    topBar and misc

---
+++ components/AppMenu.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+
+    export let name: string;
+
+</script>
+
+<div class="appMenu">
+    <button class="appMenuBtn">{name}</button>
+    <div class="menuContent">
+      <slot />
+    </div>
+  </div>
