6d56f2d23421f99f8be4a1de17aa0c4cf4f81010
Fynn Grotehans <68659993+Fynn-G@users.noreply.github.com>
2023-03-02 18:03:31 +0100

    remove test app

--- test/components/windowElements/SideBarTop.tsx
+++
@@ -1,5 +0,0 @@
-export default function () {
-    return <div class="sideBarTop" data-tauri-drag-region>
-
-    </div>;
-}
--- test/components/windowElements/ToolBarButton.tsx
+++
@@ -1,7 +0,0 @@
-export default function (props:any) {
-    return (
-        <button class="toolBarButton" onClick={props.event}>
-            <object class="icon" data={props.icon}></object>
-        </button>
-    );
-}
