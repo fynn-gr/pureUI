470d14116cbe7dee078e5980e0391874e9aa525c
Fynn Grotehans <68659993+Fynn-G@users.noreply.github.com>
2023-04-11 03:01:53 +0200

    topBar rename, icon sort/rename, remove buildsystem(gulp)

---
+++ components/TopBarButton.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+
+    export let icon: string;
+    export let onClick: Function
+    export let toolTip: string;
+    export let disabled = false;
+
+</script>
+
+<button
+    on:click={!disabled ? onClick() : null}
+    disabled={disabled}
+>
+
+    <img src={icon} alt={toolTip}>
+
+</button>
---
+++ components/TopBarToggle.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+
+	export let icon: string;
+	export let iconActive: string = null;
+	export let active: boolean;
+	export let activeColor: string = null;
+	export let toolTip: string;
+	export let disabled = false;
+
+</script>
+
+<button
+	on:click={() => {!disabled ? active = !active : active = active}}
+	disabled={disabled}
+	style={`background-color: ${active ? activeColor : 'none'};`}
+>
+
+	{#if active}
+		<img
+			src={iconActive != null ? iconActive : icon}
+			alt={toolTip}
+		>
+	{:else}
+		<img
+			src={icon}
+			alt={toolTip}
+		>
+	{/if}
+
+</button>
