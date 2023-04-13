88a671c5eeceeb9ef21fa67b94cc1c05e2991929
Fynn Grotehans <68659993+Fynn-G@users.noreply.github.com>
2023-04-13 21:25:05 +0200

    topbar toggle

--- components/TopBarToggle.svelte
+++ components/TopBarToggle.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+    import { onMount } from "svelte";
 
+	export let uiPlatform: string;
 	export let icon: string;
 	export let iconActive: string = null;
 	export let active: boolean;
@@ -7,24 +9,49 @@
 	export let toolTip: string;
 	export let disabled = false;
 
+	let self: HTMLElement;
+    let topBarType;
+    let topBarShort;
+	let iconPath;
+	let iconPathActive;
+
+    onMount(() => {
+        let cls = self;
+        while (cls.parentElement) {
+            console.log(cls, cls.classList.contains("buttonBar"))
+            cls = cls.parentElement;
+
+            if (cls.classList.contains("toolBar")) {
+                topBarType = "toolBarButton"
+                topBarShort = "tb"
+            } else if (cls.classList.contains("buttonBar")) {
+                topBarType = "buttonBarButton"
+                topBarShort = "bb"
+            }
+        }
+
+		iconPath = `../pureUI/icons/${topBarShort}_${uiPlatform}/${icon}.svg`
+
+		if (iconActive == null) {
+			iconPathActive = iconPath;
+		} else {
+			iconPathActive = `../pureUI/icons/${topBarShort}_${uiPlatform}/${iconActive}.svg`
+		}
+    })
+	
 </script>
 
 <button
+	bind:this={self}
+	class={topBarType}
 	on:click={() => {!disabled ? active = !active : active = active}}
 	disabled={disabled}
 	style={`background-color: ${active ? activeColor : 'none'};`}
 >
 
-	{#if active}
-		<img
-			src={iconActive != null ? iconActive : icon}
-			alt={toolTip}
-		>
-	{:else}
-		<img
-			src={icon}
-			alt={toolTip}
-		>
-	{/if}
+	<img
+		src={active ? iconPathActive : iconPath}
+		alt={toolTip}
+	>
 
 </button>
