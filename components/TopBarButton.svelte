1f56b7ea9f184c2242b21df99464691017a2ded7
Fynn Grotehans <68659993+Fynn-G@users.noreply.github.com>
2023-04-13 20:47:38 +0200

    TopbarButton, icon

--- components/TopBarButton.svelte
+++ components/TopBarButton.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import Icon from './Icon.svelte';
+    import { onMount } from "svelte";
 
     export let uiPlatform: string;
     export let icon: string;
@@ -7,13 +7,37 @@
     export let toolTip: string;
     export let disabled = false;
 
+    let self: HTMLElement;
+    let topBarType;
+    let topBarShort;
+
+
+    onMount(() => {
+        let cls = self;
+        while (cls.parentElement) {
+            console.log(cls, cls.classList.contains("buttonBar"))
+            cls = cls.parentElement;
+
+            if (cls.classList.contains("toolBar")) {
+                topBarType = "toolBarButton"
+                topBarShort = "tb"
+            } else if (cls.classList.contains("buttonBar")) {
+                topBarType = "buttonBarButton"
+                topBarShort = "bb"
+            }
+        }
+
+    })
+
 </script>
 
 <button
+    bind:this={self}
+    class={topBarType}
     on:click={!disabled ? onClick() : null}
     disabled={disabled}
 >
 
-    <Icon {icon} {uiPlatform}/>
+    <img src={`../pureUI/icons/${topBarShort}_${uiPlatform}/${icon}.svg`} alt={toolTip}>
 
 </button>
