5f8f2768a0d8d3a543c3ca6b04800139cc6e2884
Fynn Grotehans <68659993+Fynn-G@users.noreply.github.com>
2023-10-16 23:58:14 +0200

    app menu win

---
+++ components/AppMenuItem.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { emit } from "@tauri-apps/api/event";
+
+    export let id: string;
+    export let name: string;
+    export let accelerator: string;
+
+</script>
+
+<button
+    class="app-menu-item"
+    on:click={() => {emit("menu", id)}}
+>
+    <p class="name">{name}</p>
+    <p class="accelerator">{accelerator}</p>
+</button>
