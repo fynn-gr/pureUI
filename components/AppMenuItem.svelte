ff99a2e220b13ec61354c701dfd45ec1f8315966
Fynn Grotehans <68659993+Fynn-G@users.noreply.github.com>
2023-11-01 00:38:05 +0100

    appmenu icon, checked, disabled

--- components/AppMenuItem.svelte
+++ components/AppMenuItem.svelte
@@ -3,15 +3,28 @@
 
 	export let id: string;
 	export let name: string;
-	export let accelerator: string;
+	export let accelerator: string = null;
+	export let disabled: boolean = false;
+	export let icon: string = null;
+	export let checked: string = null;
 </script>
 
 <button
 	class="app-menu-item"
+	class:disabled
 	on:click={() => {
 		emit("menu", id);
 	}}
 >
+	{#if checked == "true"}
+		<img src="./icons/menu_win/checked.svg" alt="" />
+	{:else if checked == "false"}
+		<img src="" alt="" style="visibility: hidden;" />
+	{:else if icon != null}
+		<img src={icon} alt="" />
+	{/if}
 	<p class="name">{name}</p>
-	<p class="accelerator">{accelerator}</p>
+	{#if accelerator != null}
+		<p class="accelerator">{accelerator}</p>
+	{/if}
 </button>
