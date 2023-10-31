3b9f5d558dc8bb299396339e26754a6c253900cc
Fynn Grotehans <68659993+Fynn-G@users.noreply.github.com>
2023-10-31 20:57:47 +0100

    context Menu

---
+++ components/ContextMenu.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { contextMenu } from "@/stores";
+
+	function clickOutside(node: any) {
+		const handleClick = (event: any) => {
+			if (node && !node.contains(event.target) && !event.defaultPrevented) {
+				node.dispatchEvent(new CustomEvent("click_outside", node));
+			}
+		};
+
+		document.addEventListener("click", handleClick, true);
+
+		return {
+			destroy() {
+				document.removeEventListener("click", handleClick, true);
+			},
+		};
+	}
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div
+	class="context-menu"
+	style={`inset: ${$contextMenu.position.y}px auto auto ${$contextMenu.position.x}px;`}
+	use:clickOutside
+	on:click_outside={() => {
+		$contextMenu = null;
+	}}
+	on:click={e => {
+		$contextMenu = null;
+	}}
+>
+	{#each $contextMenu.content as item}
+		<button class="item" on:click={item.action}>
+			{item.name}
+		</button>
+	{/each}
+</div>
