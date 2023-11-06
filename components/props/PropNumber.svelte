db0249600aa7820c25dd69104a3277a42b740748
Fynn Grotehans <68659993+Fynn-G@users.noreply.github.com>
2023-11-06 22:04:52 +0100

    prop number

--- components/props/PropNumber.svelte
+++ components/props/PropNumber.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { isEditing } from "@/stores";
+
 	export let value: number;
 	export let min: number;
 	export let max: number;
@@ -6,18 +8,28 @@
 	export let softMax: number = null;
 	export let decimalDisplay: number = 2;
 	export let decimalValue: number = null;
+	export let step: number = null;
 	export let unit = "";
 	export let inactive = false;
+	export let onFocus = () => {};
+	export let onBlur = () => {};
 
 	function handleChange() {}
 </script>
 
-<input
-	class="prop"
-	type="number"
-	disabled={inactive}
-	on:change={handleChange}
-	bind:value
-	{max}
-	{min}
-/>
+<div
+	class="prop-number"
+>
+	<p class="display">{Math.round(value)}{unit}</p>
+	<input
+		type="number"
+		disabled={inactive}
+		on:change={handleChange}
+		bind:value
+		{step}
+		{max}
+		{min}
+		on:focus={onFocus}
+		on:blur={onBlur}
+	/>
+</div>
