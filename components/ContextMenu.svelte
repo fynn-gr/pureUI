91fd22a6f7206185b863521889bd82c4608379b9
Fynn Grotehans <68659993+Fynn-G@users.noreply.github.com>
2023-11-09 21:47:55 +0100

    topbar popover

--- components/ContextMenu.svelte
+++ components/ContextMenu.svelte
@@ -1,21 +1,8 @@
 <script lang="ts">
 	import { contextMenu } from "@/stores";
+	import { clickOutside } from "@/utils";
 
-	function clickOutside(node: any) {
-		const handleClick = (event: any) => {
-			if (node && !node.contains(event.target) && !event.defaultPrevented) {
-				node.dispatchEvent(new CustomEvent("click_outside", node));
-			}
-		};
 
-		document.addEventListener("click", handleClick, true);
-
-		return {
-			destroy() {
-				document.removeEventListener("click", handleClick, true);
-			},
-		};
-	}
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
---
+++ components/TopBarPopover.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+
+    import { onMount } from "svelte";
+	import { uiPlatform } from "@/stores";
+	import { clickOutside } from "@/utils";
+
+	export let icon: string;
+	export let toolTip: string;
+	export let disabled = false;
+
+	let self: HTMLElement;
+	let topBarType;
+	let topBarShort;
+    let exposed = false;
+
+	onMount(() => {
+		let cls = self;
+		while (cls.parentElement) {
+			cls = cls.parentElement;
+
+			if (cls.classList.contains("toolbar")) {
+				topBarType = "toolbar-button";
+				topBarShort = "tb";
+			} else if (cls.classList.contains("buttonbar")) {
+				topBarType = "buttonbar-button";
+				topBarShort = "bb";
+			}
+		}
+	});
+
+</script>
+
+<div
+    class="topbar-popover"
+    class:exposed={exposed}
+    use:clickOutside
+    on:clickOutside={() => {
+        exposed = false;
+        console.log("clicked outside")
+    }}
+>
+    <button
+        bind:this={self}
+        class="topbar-button"
+        class:disabled
+        on:click={e => {
+            exposed = !exposed;
+        }}
+        {disabled}
+        title={toolTip}
+    >
+        <img
+            src={topBarShort ? `./icons/${topBarShort}_${$uiPlatform}/${icon}.svg` : ""}
+            alt={toolTip}
+        />
+    </button>
+
+    {#if exposed}
+        <div class="popover">
+            <slot />
+        </div>
+    {/if}
+</div>
