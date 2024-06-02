dbdd8917241442f0c9df1608dc155fe2a2847b4c
Fynn Grotehans <68659993+Fynn-G@users.noreply.github.com>
2024-06-02 02:03:07 +0200

    topbar adjstments 1

---
+++ components/TopBarWebMenu.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+import { onMount } from "svelte";
+import { clickOutside } from "@/pureUI/modules/utils";
+
+let self: HTMLElement;
+let exposed = false;
+
+onMount(() => {
+
+});
+</script>
+
+<div
+	class="topbar-webmenu"
+	class:exposed
+	use:clickOutside={() => {
+		exposed = false;
+	}}
+>
+	<button
+		bind:this={self}
+		class="topbar-button"
+		on:click={e => {
+			exposed = !exposed;
+		}}
+		title={"App Menu"}
+	>
+		<p class="logo">poly <b>Refsheet</b></p>
+	</button>
+
+	{#if exposed}
+		<div class="popover">
+			<a href="">Home</a>
+            <a href="">Fairytale</a>
+		</div>
+	{/if}
+</div>
