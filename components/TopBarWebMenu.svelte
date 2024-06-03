a17d0a7b7a3451b17587d734bd8063cb316c582b
Fynn Grotehans <68659993+Fynn-G@users.noreply.github.com>
2024-06-03 02:06:46 +0200

    topbar adjustemnts 3; web icons; web menu

--- components/TopBarWebMenu.svelte
+++ components/TopBarWebMenu.svelte
@@ -2,6 +2,7 @@
 import { onMount } from "svelte";
 import { clickOutside } from "@/pureUI/modules/utils";
 
+export let active: string;
 let self: HTMLElement;
 let exposed = false;
 
@@ -16,20 +17,41 @@ onMount(() => {});
 	}}
 >
 	<button
+		class="webmenu-btn"
 		bind:this={self}
-		class="topbar-button"
 		on:click={e => {
 			exposed = !exposed;
 		}}
-		title={"App Menu"}
+		title={"Main Menu"}
 	>
-		<p class="logo">poly <b>Refsheet</b></p>
+		<p>snug<b>Refsheet</b></p>
 	</button>
 
 	{#if exposed}
 		<div class="popover">
-			<a href="">Home</a>
-			<a href="">Fairytale</a>
+			<a href="https://github.com/Fynn-G">
+				<img src="./icons/web/home.svg" alt="" />
+				<p>Fynn Gr.</p>
+			</a>
+			<a href="">
+				<img src="./icons/web/apps.svg" alt="" />
+				<p>All Tools</p>
+			</a>
+			<a href="" class:active={active == "stagetune"}>
+				<img src="./icons/web/stagetune.svg" alt="" />
+				<p>Stragetune</p>
+				<p class="tag" style="background-color: #FF2D55;">Beta</p>
+			</a>
+			<a href="" class:active={active == "fairytale"}>
+				<img src="./icons/web/fairytale.svg" alt="" />
+				<p>Fairytale</p>
+				<p class="tag" style="background-color: #FF9500;">Alpha</p>
+			</a>
+			<a href="" class:active={active == "refsheet"}>
+				<img src="./icons/web/refsheet.svg" alt="" />
+				<p>Refsheet</p>
+				<p class="tag" style="background-color: #4F4F4F;">Alpha</p>
+			</a>
 		</div>
 	{/if}
 </div>
