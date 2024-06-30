71087b95623bd5b6e651321a00ebcd6d6502ead0
Fynn Grotehans <68659993+Fynn-G@users.noreply.github.com>
2024-06-30 14:43:18 +0200

    seperate seettings options

---
+++ components/settings/SettingsCheckbox.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+
+export let name: string;
+export let checked: boolean;
+export let checkboxName: string;
+export let onChange = () => {};
+
+</script>
+
+<div class="option">
+	<p class="name">{name}</p>
+	<input type="checkbox" bind:checked on:change={onChange} />
+	<p class="checkbox-name">{checkboxName}</p>
+</div>
---
+++ components/settings/SettingsSelect.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+import { uiPlatform } from "@/ts/Stores";
+
+export let name: string;
+export let value: any;
+export let options: Array<selectionOption>;
+export let onChange = () => {};
+
+type selectionOption = { value: any; name: string };
+
+</script>
+
+<div class="option">
+	<p class="name">{name}</p>
+	<div class="select-container">
+		<select bind:value on:change={onChange}>
+			{#each options as option}
+				<option value={option.value}>{option.name}</option>
+			{/each}
+		</select>
+		{#if $uiPlatform == "mac"}
+			<div class="arrow">
+				<img src="./icons/std/select_arrow.svg" alt="" />
+			</div>
+		{/if}
+	</div>
+</div>
