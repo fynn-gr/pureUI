926d09e8d3da451da740366b206d3e15d9ad4b23
Fynn Grotehans <68659993+Fynn-G@users.noreply.github.com>
2024-07-02 21:28:08 +0200

    navigation list design, slim, folder

---
+++ components/NavItem.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+export let name: string;
+export let icon: string | null = null;
+export let onClick: Function;
+export let active: boolean;
+</script>
+
+<button
+	class="nav-item"
+	class:active
+	on:click={() => {
+		active = !active;
+        onClick();
+	}}
+>
+	{#if icon}
+		<img src={`./icons/tool_bar/${icon}.svg`} alt="" class="icon" />
+	{/if}
+	<p>{name}</p>
+</button>
