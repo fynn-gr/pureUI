a9a70135701a8f0d9fec2254fcf4417e2704bc0b
Fynn Grotehans <68659993+Fynn-G@users.noreply.github.com>
2024-09-24 00:49:33 +0200

    prop number

--- components/props/PropNumber.svelte
+++ components/props/PropNumber.svelte
@@ -1,27 +1,27 @@
 <script lang="ts">
-import { isEditing } from "@/ts/Stores";
-
 export let value: number;
 export let min: number;
 export let max: number;
-export let softMin: number = null;
-export let softMax: number = null;
+export let softMin: number = min;
+export let softMax: number = max;
 export let decimalDisplay: number = 2;
-export let decimalValue: number = null;
-export let step: number = null;
+export let step: number = 1;
 export let unit = "";
-export let inactive = false;
+export let disabled = false;
 export let onFocus = () => {};
 export let onBlur = () => {};
+export let title: string;
 
 function handleChange() {}
+
+$: dispValue = Math.round(value * decimalDisplay) / decimalDisplay
 </script>
 
 <div class="prop-number">
-	<p class="display">{Math.round(value)}{unit}</p>
+	<p class="display">{dispValue} {unit}</p>
 	<input
 		type="number"
-		disabled={inactive}
+		{disabled}
 		on:change={handleChange}
 		bind:value
 		{step}
@@ -30,4 +30,20 @@ function handleChange() {}
 		on:focus={onFocus}
 		on:blur={onBlur}
 	/>
+	<span>
+		<button
+			on:click={() => {
+				value = value + step;
+			}}>
+				<img class="arrow-icon" src="./icons/std/number_arrow_up.svg" alt="">
+			</button
+		>
+		<button
+			on:click={() => {
+				value = value - step;
+			}}>
+				<img class="arrow-icon" src="./icons/std/number_arrow_down.svg" alt="">
+			</button
+		>
+	</span>
 </div>
