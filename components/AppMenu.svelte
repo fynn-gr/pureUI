0a3abcac92757b26f0979b45f75276452389e92f
Fynn Grotehans <68659993+Fynn-G@users.noreply.github.com>
2024-10-14 23:09:06 +0200

    menu change hover to click

--- components/AppMenu.svelte
+++ components/AppMenu.svelte
@@ -1,10 +1,25 @@
 <script lang="ts">
+import { clickOutside } from "../modules/utils";
+
 export let name: string;
+let exposed: boolean = false;
+
+function handleOpenMenu(e) {
+	exposed = !exposed;
+}
 </script>
 
-<div class="app-menu">
-	<button class="app-menu-btn">{name}</button>
-	<div class="menu-content">
-		<slot />
-	</div>
+<div
+	class="app-menu"
+	use:clickOutside={() => {
+		exposed = false;
+		console.log("c outside");
+	}}
+>
+	<button on:click={handleOpenMenu} class="app-menu-btn">{name}</button>
+	{#if exposed}
+		<div class="menu-content">
+			<slot />
+		</div>
+	{/if}
 </div>
