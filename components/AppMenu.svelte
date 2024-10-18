f4d756723030d882ddb53d50a768209eb8738170
Fynn Grotehans <68659993+Fynn-G@users.noreply.github.com>
2024-10-18 17:23:05 +0200

    init menu container

--- components/AppMenu.svelte
+++ components/AppMenu.svelte
@@ -1,25 +1,14 @@
 <script lang="ts">
-import { clickOutside } from "../modules/utils";
 
 export let name: string;
-let exposed: boolean = false;
 
-function handleOpenMenu(e) {
-	exposed = !exposed;
-}
 </script>
 
 <div
 	class="app-menu"
-	use:clickOutside={() => {
-		exposed = false;
-		console.log("c outside");
-	}}
 >
-	<button on:click={handleOpenMenu} class="app-menu-btn">{name}</button>
-	{#if exposed}
+	<button class="app-menu-btn">{name}</button>
 		<div class="menu-content">
 			<slot />
 		</div>
-	{/if}
 </div>
