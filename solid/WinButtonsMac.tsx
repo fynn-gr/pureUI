c20b5123bbe95ef424469c284dfd7ca98cd81462
Fynn Grotehans <68659993+Fynn-G@users.noreply.github.com>
2025-02-25 02:17:15 +0100

    solid init

---
+++ solid/WinButtonsMac.tsx
@@ -0,0 +1,17 @@
+function WinButtonsMac() {
+	return (
+		<div class="win-buttons-mac" data-tauri-drag-region>
+			<button>
+				<img src="./icons/native/winButtonsMacClose.svg" alt="" />
+			</button>
+			<button>
+				<img src="./icons/native/winButtonsMacMin.svg" alt="" />
+			</button>
+			<button>
+				<img src="./icons/native/winButtonsMacMax.svg" alt="" />
+			</button>
+		</div>
+	);
+}
+
+export default WinButtonsMac;
