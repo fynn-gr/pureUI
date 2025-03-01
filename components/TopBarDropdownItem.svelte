a0161ba3ade67660a1200954abfc384e427ba6ca
Fynn Grotehans <68659993+Fynn-G@users.noreply.github.com>
2025-03-01 17:39:27 +0100

    dropdown item features

--- components/TopBarDropdownItem.svelte
+++ components/TopBarDropdownItem.svelte
@@ -3,18 +3,18 @@ interface Props {
 	name: string;
 	checked?: boolean;
 	onChange?: Function;
+	disabled?: boolean;
 }
-let { name, checked = $bindable(), onChange = () => {} }: Props = $props();
+let { name, checked = $bindable(), onChange = () => {}, disabled = false}: Props = $props();
 </script>
 
-<button
-	class="item"
-	onclick={() => {
+<button class="item" class:disabled={disabled} onclick={() => {
 		//checked = !checked;
 		onChange();
-	}}
->
-	{#if checked == true}
+	}}>
+	{#if checked == undefined}
+		<img src="" alt="" style="visibility: hidden;" />
+	{:else if checked == true}
 		<img src="./icons/app_menu/checked.svg" alt="" />
 	{:else if checked == false}
 		<img src="" alt="" style="visibility: hidden;" />
