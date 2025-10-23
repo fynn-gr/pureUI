4b3b0ba9e55d5cd96671f750e4e65542f03b0a4c
Fynn Grotehans <68659993+Fynn-G@users.noreply.github.com>
2025-10-24 00:13:50 +0200

    tahoe

---
+++ components/Sidebar.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+
+interface Props {
+  width: number;
+  show: boolean;
+}
+let { width, show }: Props = $props();
+</script>
+
+<div class="sidebar" style={`width: ${show ? width : 0}px;`}>
+  <div class="siebar-inner">
+    <slot />
+  </div>
+</div>
