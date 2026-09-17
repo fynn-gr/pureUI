0d3cce46016742c08bffb57351e54bbfa81f0b13
Fynn Grotehans <68659993+Fynn-G@users.noreply.github.com>
2026-09-17 02:11:50 +0200

    remove redundant usingtauri

--- modules/utils.ts
+++ modules/utils.ts
@@ -77,11 +77,3 @@ export function clickOutside(element: HTMLElement, callbackFunction: Function) {
 		},
 	};
 }
-
-export function usingTauri(): boolean {
-	return (
-		typeof window !== "undefined" &&
-		typeof (window as Window & { __TAURI__?: unknown }).__TAURI__ !==
-			"undefined"
-	);
-}
